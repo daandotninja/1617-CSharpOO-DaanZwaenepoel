ed783cc5950ddffcd63b9486e8b2e43342034728
Daan Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2016-10-25 18:06:31 +0200

    24/25

--- Opgave03/LogicLayer/LogicImplementation.cs
+++ Opgave03/LogicLayer/LogicImplementation.cs
@@ -11,37 +11,72 @@ namespace LogicLayer
     {
         private readonly IDataAccess dataAccessLayer;
         public DateTime CurrentDate { get; }
+        public Dictionary<DateTime, RunInfo> storeInfo = new Dictionary<DateTime, RunInfo>();
         public LogicImplementation(IDataAccess backend)
         {
             dataAccessLayer = backend;
+            CurrentDate = DateTime.Now.Date;
         }
         public void ClearData()
         {
-
+            dataAccessLayer.ClearData();
         }
         public void Close()
         {
-
+            dataAccessLayer.Close();
         }
         public RunInfo GetRunInfoForDate(DateTime date)
         {
-            return null;
+            return dataAccessLayer.GetRunInfoForDate(date);
         }
         public RunInfo[] GetRunInfoForPastSevenDays()
         {
-            return null;
+            RunInfo[] days = new RunInfo[7];
+            for (int i = 0; i < 6 ; i++)
+            {
+                
+
+                days[i]= GetRunInfoForDate(CurrentDate.AddDays(-6 + i));
+
+            }
+            days[6] = GetRunInfoForDate(CurrentDate);
+            return days;
         }
         public RunInfo[] GetRunInfoForPastThirtyDays()
         {
-            return null;
+            RunInfo[] days = new RunInfo[30];
+            for (int i = 0; i < 29; i++)
+            {
+
+
+                days[i] = GetRunInfoForDate(CurrentDate.AddDays(-29 + i));
+
+            }
+            days[29] = GetRunInfoForDate(CurrentDate);
+            return days;
         }
         public double GetSpeedInKmPerHour(RunInfo info)
         {
-            return 0.0;
+            return ((double)info.DistanceInMeter / (double)((info.Interval.Hours*3600)+(info.Interval.Minutes*60)+info.Interval.Seconds))*3.6;
         }
         public void SetRunInfoForDate(DateTime date, RunInfo info)
         {
+            if (info.DistanceInMeter >= 0)
+            {
+                if(GetSpeedInKmPerHour(info) >= 50.0)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                else
+                {
+                    dataAccessLayer.StoreRunInfo(date, info);
+                }
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException();
 
+            }
         }
     }
 }
