a7b6b9a6416619e0590ccef1654a641eb2875e94
Daan Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2016-10-25 18:41:58 +0200

    24/25 still

--- Opgave03/LogicLayer/LogicImplementation.cs
+++ Opgave03/LogicLayer/LogicImplementation.cs
@@ -69,6 +69,7 @@ namespace LogicLayer
                 }
                 else
                 {
+                     
                     dataAccessLayer.StoreRunInfo(date, info);
                 }
             }
