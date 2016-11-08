6890b0ab7b79c588e9cdd47d3ac89ee83ddb8903
Daan Zwaenepoel <daan.zwaenepoel@student.odisee.be>
2016-11-08 16:01:24 +0100

    3b 27 pass

---
+++ Opgave03/RunnerMain/MainForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Globals;
+
+namespace RunnerMain
+{
+    public partial class MainForm : Form
+    {
+        private readonly ILogic logic;
+        public MainForm(ILogic logic)
+        {
+            InitializeComponent();
+            this.logic = logic;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Opgave03/UnitTestProject1/UnitTest4.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using Globals;
+using LogicLayer;
+using RunnerMain;
+
+namespace UnitTests
+{
+
+    [TestClass]
+    public class DeelB_Architectuur
+    {
+        [TestMethod, Timeout(100)]
+        public void TestIfMainFormConstructorTakesLogicInterfaceParameter()
+        {
+            Type x = typeof(MainForm);
+            ConstructorInfo constructor = x.GetConstructor(new Type[] { typeof(ILogic) });
+            Assert.IsNotNull(constructor,
+                $"Deel B_Architectuur - constructor for \"MainForm\" does not take ILogic parameter!");
+            constructor = x.GetConstructor(new Type[] { });
+            Assert.IsNull(constructor,
+                $"Deel B_Architectuur - \"MainForm\" has a default constructor (not allowed)!");            
+        }
+
+        [TestMethod, Timeout(100)]
+        public void TestIfMainFormHasAPrivateIlogicfield()
+        {
+            var fields = typeof(MainForm).GetFields();
+            var OK = (fields.Where(f => f.FieldType == typeof(ILogic)).Count() == 0);
+            Assert.IsTrue(OK, $"Deel B_Architectuur - \"MainForm\" uses a public field to store \"ILogic\" reference (must be private)!");
+            fields = typeof(MainForm).GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance);
+            OK = (fields.Where(f => f.FieldType == typeof(ILogic)).Count() > 0);
+            Assert.IsTrue(OK, $"Deel B_Architectuur - \"MainForm\" does not have a private field to store ILogic reference)!");            
+        }
+        
+        [TestMethod, Timeout(100)]
+        public void CheckMainFormhasNoFieldsOfTypeLogicImplementation()
+        {
+            Type x = typeof(MainForm);
+            ConstructorInfo constructor = x.GetConstructor(new Type[] { typeof(ILogic) });
+            Assert.IsNotNull(constructor,
+                $"Deel B_Architectuur - constructor for \"MainForm\" does not take ILogic parameter!");
+            constructor = x.GetConstructor(new Type[] { });
+            Assert.IsNull(constructor,
+                $"Deel B_Architectuur - \"MainForm\" has a default constructor (not allowed)!");
+            var fields = typeof(MainForm).GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance);
+            var OK = (fields.Where(f => f.FieldType == typeof(LogicImplementation)).Count() == 0);
+            Assert.IsTrue(OK, $"Deel B_Architectuur - \"MainForm\" uses a private instance of \"logicImplementation\" (must use injected interface instead)!");
+            fields = typeof(MainForm).GetFields();
+            OK = (fields.Where(f => f.FieldType == typeof(LogicImplementation)).Count() == 0);
+            Assert.IsTrue(OK, $"Deel B_Architectuur - \"MainForm\" uses a public instance of \"logicImplementation\" (must use injected interface instead)!");
+            fields = typeof(MainForm).GetFields(BindingFlags.NonPublic |
+                         BindingFlags.Instance);
+        }
+    }
+}
